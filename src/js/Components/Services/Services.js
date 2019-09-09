bd1360fcc22ec074119a4575a65aeff70071ed96
Tim Gorsuch <gorsuchtim@gmail.com>
2019-09-09 15:39:34 -0500

    add import for flashtile

--- src/js/Components/Services/Services.js
+++ src/js/Components/Services/Services.js
@@ -5,6 +5,7 @@ import CreateBlock from "../BuildGrid/CreateBlock";
 import BuildGrid from "../BuildGrid/BuildGrid";
 import Countdown from "../Countdown/Countdown";
 import DropBlocks from "../DropBlocks/DropBlocks";
+import FlashTile from "../FlashTile/FlashTile";
 
 const Services = {
   init() {
