cffd8cc564b1c82ff11c123b5c7fef953e85a0ee
Tim Gorsuch <gorsuchtim@gmail.com>
2019-09-22 15:38:35 -0500

    move settilesize out of services, add reset behavior to app

--- src/js/Components/Services/Services.js
+++ src/js/Components/Services/Services.js
@@ -4,14 +4,11 @@ import BuildGrid from "../BuildGrid/BuildGrid";
 import DropBlocks from "../DropBlocks/DropBlocks";
 import FlashTile from "../FlashTile/FlashTile";
 import SetGridSize from "../SetGridSize/SetGridSize";
-import SetTileSize from "../SetTileSize/SetTileSize";
 
 const Services = {
   init() {
     if (SetGridSize.init()) {
-      if (SetTileSize.init()) {
-        BuildGrid();
-      }
+      BuildGrid();
     }
   },
   runGame() {
